841c051fa55a2613eeba5aeb173a6cf4da3a6484
lorenzodelmedico <lorenzodelmedico@gmail.com>
2023-06-04 17:01:50 +0200

    frontend home ok

--- template/serveur.py
+++ template/serveur.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
+app = Flask(__name__, template_folder='templates', static_folder='staticFiles', )
 
 @app.route('/')
 def home():
     
-    items = ["LexRank", "TextRank", "LSA", "Bart"]
+    items = ["LSA","LexRank", "TextRank"]
     summarize_text = "Le résultat va s'afficher ici...!"
     return render_template('home.html', items=items, summarize_text = summarize_text)
+
+def aboutus():
+    
+    return render_template('aboutus.html')
+
+def documentation():
+
+    return render_template('documentation.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
