420f9d2c9e8bbab882d155f0934c1b82fc0afe56
Osmonoz <Osmonoz@protonmail.com>
2023-06-05 00:29:36 +0200

    feat(multiprocess): multiprocess is now working

--- src/server/server.py
+++ src/server/server.py
@@ -1,11 +1,13 @@
 import uuid
-from multiprocessing import Process
+from multiprocessing import Process, Manager, Queue
 from flask import Flask, request, abort
 from media_transcript_remeetai import process_media, text_summarization
 
+from media_transcript_remeetai.text_summarization import bart_summarization, lexrank_summarization, textrank_lsa
+
 # threaded.ThreadPooled.configure(max_workers=3)
 
-thread = {}
+process_registry = {}
 
 app = Flask(__name__)
 
@@ -13,17 +15,29 @@ app = Flask(__name__)
 def hello_world():
   return "<p>Hello, World!</p>"
 
-def run_subprocess(tool, filename, uid):
-  transcript, _, _, _ = process_media.trancript(filename)
-  if tool == "bart":
-    report = text_summarization.bart_sum(text_base = transcript)
-  elif tool == "lexrank":
-    report = text_summarization.lexrank_sum(text_base = transcript)
-  elif tool == "textrank_lsa":
-    report = text_summarization.textrank_lsa_sum(text_base = transcript)
+def run_subprocess(tool, filename, uid, data):
+  # data = {}
+  try:
+    transcript, _, _, _ = process_media.trancript(filename)
+    if tool == "bart":
+      report = bart_summarization.bart_sum(text_base = transcript)
+    elif tool == "lexrank":
+      report = lexrank_summarization.lexrank_sum(text_base = transcript)
+    elif tool == "textrank_lsa":
+      report = textrank_lsa.textrank_lsa_sum(text_base = transcript)
 
-  thread[uid]["report"] = report
-  thread[uid]["is_done"] = True
+    # thread[uid]["report"] = report
+    # thread[uid]["is_done"] = True
+    # thread[uid]["error"] = False
+    data["report"] = report
+    data["is_done"] = True
+    data["error"] = False
+    print(f"{uid} => done")
+  except Exception as e:
+    data["is_done"] = True
+    data["error"] = True
+    data["msg"] = str(e.args[0])
+    print(f"{uid} => error")
 
 
 @app.post("/summarize/")
@@ -34,25 +48,31 @@ def summarize(tool = "bart"):
   f = request.files['file']
   f.save(f'../../res/data/{f.filename}')
   uid = uuid.uuid1()
-  process = Process(target=run_subprocess, args=(tool, f'../../res/data/{f.filename}', uid))
+  manager = Manager()
+  thread = manager.dict()
+  thread["is_done"] = False
+  process = Process(target=run_subprocess, args=(tool, f'../../res/data/{f.filename}', str(uid), thread))
   process.start()
-  thread[str(uid)] = {
-    "process": process,
-    "is_done": False
-  }  
+  process_registry[str(uid)] = {"p": process, "data": thread} 
   return {"id": f"{uid}"}
   
 
 @app.get("/summary/<uid>")
 def get_summary(uid):
   print(uid)
-  print(thread)
   try:
-    th = thread[uid]
-  except:
-    return {"error": "uid not exist."}, 404
-  if th["is_done"]:
+    print(process_registry[uid]["p"].exitcode)
+    if process_registry[uid]["p"].exitcode != None:
+      process_registry[uid]["p"].join()
+    print(process_registry[uid]["data"])
+    print(process_registry[uid]["p"])
+    th = process_registry[uid]["data"]
+  except Exception as e:
+    return {"error": str(e)}, 404
+  if th["is_done"] and not th["error"]:
     return {"done": True, "report": th["report"]}
+  elif th["is_done"]:
+    return {"error": f"internal error: {th['msg']}"}
   return {"done": False, "message": "still processing..."}
 
 app.run(port=8000)
